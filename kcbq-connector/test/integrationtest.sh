0a0e7025ae2de5fcaeb47af1b682620744e0ef00
Moira Tagle <moirat@wepay.com>
2018-09-06 15:48:52 -0700

    Fix some things, update README slightly.

--- kcbq-connector/test/integrationtest.sh
+++ kcbq-connector/test/integrationtest.sh
@@ -233,9 +233,8 @@ echo >> "$CONNECTOR_PROPS"
 CONNECT_DOCKER_IMAGE='kcbq/connect'
 CONNECT_DOCKER_NAME='kcbq_test_connect'
 
-# todo fixme
-cp "$BASE_DIR"/../../kcbq-confluent/build/distributions/kafka-connect-bigquery-*-SNAPSHOT.tar "$DOCKER_DIR/connect/kcbq.tar"
-cp "$BASE_DIR"/../../kcbq-confluent/build/distributions/kafka-connect-bigquery-*-SNAPSHOT.tar "$DOCKER_DIR/connect/retriever.tar"
+cp "$BASE_DIR"/../../kcbq-confluent/build/distributions/kafka-connect-bigquery-*.tar "$DOCKER_DIR/connect/kcbq.tar"
+cp "$BASE_DIR"/../../kcbq-confluent/build/distributions/kafka-connect-bigquery-*.tar "$DOCKER_DIR/connect/retriever.tar"
 cp "$KCBQ_TEST_KEYFILE" "$DOCKER_DIR/connect/key.json"
 
 if ! dockerimageexists "$CONNECT_DOCKER_IMAGE"; then
