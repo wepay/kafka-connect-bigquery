57d2bc0c11d378f879a2d948f76d17d3eba03a4b
Naya Chen <nayachen@nchen-MBP13.local>
2019-08-08 23:01:05 -0700

    change sanitize behavior

--- kcbq-connector/src/main/java/com/wepay/kafka/connect/bigquery/utils/FieldNameSanitizer.java
+++ kcbq-connector/src/main/java/com/wepay/kafka/connect/bigquery/utils/FieldNameSanitizer.java
@@ -5,19 +5,26 @@ import java.util.stream.Collectors;
 
 public class FieldNameSanitizer {
 
+  // replace all non-letter, non-digit characters with underscore
+  // append underscore in front of name if it does not begin with letter or underscore
   private static String sanitizeName(String name) {
-    name = name.replaceAll("^[^a-zA-Z_]+", "");
-    return name.replaceAll("[^a-zA-Z0-9_]", "_");
+    name = name.replaceAll("[^a-zA-Z0-9_]", "_");
+    if (name.matches("^[^a-zA-Z_]+.*")) {
+      name = "_" + name;
+    }
+    return name;
   }
 
 
   // Big Query specifies field name must begin with a letter or underscore and can only contain
   // letters, numbers, and underscores
+  // Note that a.b and a/b will have the same value after sanitization
+  // which will cause Duplicate key Exception
   public static Map<String,Object> replaceInvalidKeys(Map<String, Object> map) {
 
     return map.entrySet().stream().collect(Collectors.toMap(
             (entry) -> {
-              return sanitizeName((String) (entry.getKey()));
+              return sanitizeName(entry.getKey());
             },
             (entry) -> {
               if (entry.getValue() instanceof Map) {
--- kcbq-connector/src/test/java/com/wepay/kafka/connect/bigquery/utils/FieldNameSanitizerTest.java
+++ kcbq-connector/src/test/java/com/wepay/kafka/connect/bigquery/utils/FieldNameSanitizerTest.java
@@ -1,5 +1,6 @@
 package com.wepay.kafka.connect.bigquery.utils;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -10,21 +11,26 @@ import static org.junit.Assert.assertTrue;
 public class FieldNameSanitizerTest {
   HashMap testMap;
 
+  @Before
+  public void setUp() {
+    testMap =
+        new HashMap<String, HashMap<?, ?>>() {{
+          put("A.1", new HashMap<String, Object>() {{
+            put("_B1", 1);
+            put("B.2", "hello.B-2");
+          }});
+          put("A-2", new HashMap<String, Object>() {{
+            put("=/B.3", "hello B3");
+            put("B./4", "hello B4");
+            put("2A/", "hello B5");
+            put("3A/", "hello B6");
+          }});
+        }};
+  }
+
 
   @Test
   public void testInvalidSymbol() {
-    testMap =
-            new HashMap<String, HashMap<?, ?>>() {{
-              put("A.1", new HashMap<String, Object>() {{
-                put("_B1", 1);
-                put("B.2", "hello.B-2");
-              }});
-              put("A-2", new HashMap<String, Object>() {{
-                put("=/B.3", "hello B3");
-                put("B./4", "hello B4");
-              }});
-            }};
-
     testMap = (HashMap)FieldNameSanitizer.replaceInvalidKeys(testMap);
     assertTrue(testMap.containsKey("A_1"));
     assertTrue(testMap.containsKey("A_2"));
@@ -34,11 +40,14 @@ public class FieldNameSanitizerTest {
     assertEquals(((HashMap)(testMap.get("A_1"))).get("B_2"), "hello.B-2");
     assertEquals(((HashMap)(testMap.get("A_1"))).get("_B1"), 1);
 
-    assertTrue(((HashMap)(testMap.get("A_2"))).containsKey("B_3"));
+    assertTrue(((HashMap)(testMap.get("A_2"))).containsKey("__B_3"));
     assertTrue(((HashMap)(testMap.get("A_2"))).containsKey("B__4"));
 
-    assertEquals(((HashMap)(testMap.get("A_2"))).get("B_3"), "hello B3");
+    assertEquals(((HashMap)(testMap.get("A_2"))).get("__B_3"), "hello B3");
     assertEquals(((HashMap)(testMap.get("A_2"))).get("B__4"), "hello B4");
+
+    assertEquals(((HashMap)(testMap.get("A_2"))).get("_2A_"), "hello B5");
+    assertEquals(((HashMap)(testMap.get("A_2"))).get("_3A_"), "hello B6");
 
   }
 
