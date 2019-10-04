6f65f32b6954fca63aff8e12b721cf435e7f3d4d
Bingqin Zhou <zhou.bing@husky.neu.edu>
2019-10-04 10:24:39 -0700

    Log the topic and table when a row fail to be inserted.

--- kcbq-connector/src/main/java/com/wepay/kafka/connect/bigquery/write/row/BigQueryWriter.java
+++ kcbq-connector/src/main/java/com/wepay/kafka/connect/bigquery/write/row/BigQueryWriter.java
@@ -188,7 +188,7 @@ public abstract class BigQueryWriter {
     List<InsertAllRequest.RowToInsert> failRows = new ArrayList<>();
     for (int index = 0; index < rows.size(); index++) {
       if (failRowsSet.contains((long)index)) {
-        logger.debug("Row with index {} from topic {} failed to be written to table {}.", index, topic, table);
+        logger.debug("Row with index {} from topic {} failed to be written to table {}.", index, topic, table.getFullTableName());
         failRows.add(rows.get(index));
       }
     }
